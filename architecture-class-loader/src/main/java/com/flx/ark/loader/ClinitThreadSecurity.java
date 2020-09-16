b322f09a3f0557ba579d07447ce7a995c688d88f
fenglixiong <fenglixiong123@163.com>
2020-09-17 01:09:59 +0800

    classloader

---
+++ architecture-class-loader/src/main/java/com/flx/ark/loader/ClinitThreadSecurity.java
@@ -0,0 +1,28 @@
+package com.flx.ark.loader;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @Author Fenglixiong
+ * @Create 2020/9/17 0:33
+ * @Description
+ * 证明每个类的clinit方法在虚拟机层面线程安全的
+ **/
+public class ClinitThreadSecurity {
+
+    //初始化阶段执行的
+    static {
+        a = 20;
+    }
+
+    //准备阶段赋值默认值为0
+    //初始化阶段赋值2
+    private static int a = 2;
+
+    public static void main(String[] args) {
+
+        System.out.println(a);
+
+    }
+
+}
