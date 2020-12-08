50eea78178041119f6ec20d325d0662100af57e5
fenglixiong <fenglixiong123@163.com>
2020-12-09 00:08:10 +0800

    AtomicIntegerTest

---
+++ architecture-multi-thread/src/main/java/com/flx/multi/thread/wangwenjun/juc/atomic/AtomicIntegerTest.java
@@ -0,0 +1,26 @@
+package com.flx.multi.thread.wangwenjun.juc.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Author Fenglixiong
+ * @Create 2020/12/8 22:38
+ * @Description
+ **/
+public class AtomicIntegerTest {
+
+    public static void main(String[] args) {
+
+        /**
+         * create
+         */
+        final AtomicInteger i = new AtomicInteger();
+        System.out.println(i.get());
+        i.set(10);
+        System.out.println(i.get());
+        int a = i.getAndAdd(6);
+        System.out.println("a = "+a);
+        System.out.println("i = "+i.get());
+    }
+
+}
