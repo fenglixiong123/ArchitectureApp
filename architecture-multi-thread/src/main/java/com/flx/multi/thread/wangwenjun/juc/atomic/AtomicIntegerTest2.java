7ff40280e45f3d8df0e52a57dd237805a728b68a
fenglixiong <fenglixiong@flashhold.com>
2021-02-23 18:29:20 +0800

    AtomicInteger 测试

--- architecture-multi-thread/src/main/java/com/flx/multi/thread/wangwenjun/juc/atomic/AtomicIntegerTest2.java
+++ architecture-multi-thread/src/main/java/com/flx/multi/thread/wangwenjun/juc/atomic/AtomicIntegerTest2.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  **/
 public class AtomicIntegerTest2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, NoSuchFieldException {
 
         /**
          * create
@@ -21,6 +21,37 @@ public class AtomicIntegerTest2 {
         int a = i.getAndAdd(6);
         System.out.println("a = "+a);
         System.out.println("i = "+i.get());
+
+        //打印AtomicInteger的字段值
+        System.out.println(AtomicInteger.class.getDeclaredField("value"));
+
+        //不安全的操作方法
+        sum();
+
+
+    }
+
+    private static int count = 0;
+
+    /**
+     * 不安全 结果不一定是10000
+     */
+    private static void sum() throws InterruptedException {
+        Thread[] ts = new Thread[10];
+        for (int i=0;i<10;i++){
+            ts[i] = new Thread(()->{
+                for (int c=0;c<1000;c++){
+                    count ++;
+                }
+            });
+            ts[i].start();
+        }
+        for (int i=0;i<10;i++){
+            ts[i].join();
+        }
+
+        System.out.println("count = "+count);
+
     }
 
 }
