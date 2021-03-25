e2c5395676992b227a3c0b1fd6c846ea23bacc94
fenglixiong <fenglixiong@flashhold.com>
2021-03-25 15:02:03 +0800

    直接缓冲区和非直接缓冲区

--- architecture-nio-netty/src/main/java/com/flx/nio/buffer/BufferDirect.java
+++ architecture-nio-netty/src/main/java/com/flx/nio/buffer/BufferDirect.java
@@ -5,6 +5,9 @@ package com.flx.nio.buffer;
  * @Date: 2021/3/25 12:22
  * @Description: 直接缓冲区和非直接缓冲区
  *
+ * 非直接缓冲区：通过allocate()方法分配的缓冲区，将缓冲区建立在jvm内存中
+ * 直接缓冲区：通过allocateDirect()方法分配的缓冲区，将缓冲区建立在物理内容中，可以提高效率
+ *
  */
 public class BufferDirect {
 }
